b8d2347c529aa1e06f67070584ee0bc83b1059c6
Murtaza Udaipurwala <murtaza.u6e24@gmail.com>
2022-03-25 14:31:20 +0530

    read base URL from environment variable

--- api/types.go
+++ api/types.go
@@ -1,6 +1,9 @@
 package api
 
-import "time"
+import (
+	"os"
+	"time"
+)
 
 type MetaData struct {
 	Expiry    time.Time `json:"expiry"`
@@ -12,4 +15,14 @@ type HTTPErr struct {
 	Status int
 }
 
-const uploadDir = "../uploads"
+type Resp struct {
+	Err string `json:"error,omitempty"`
+	URL string `json:"url,omitempty"`
+}
+
+const (
+	maxUploadSize = 1024 * 1024 * 10
+	uploadDir     = "../uploads"
+)
+
+var baseURL = os.Getenv("BASE_URL")
