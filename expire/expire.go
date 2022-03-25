1d14bb7371e054de68a23868936bf813ac1aa8fd
Murtaza Udaipurwala <murtaza.u6e24@gmail.com>
2022-03-25 20:26:18 +0530

    shred off expired files

---
+++ expire/expire.go
@@ -0,0 +1,48 @@
+package expire
+
+import (
+	"log"
+	"time"
+
+	"github.com/murtaza-udaipurwala/fs/api"
+	"github.com/murtaza-udaipurwala/fs/db"
+)
+
+func logErr(ctx, err string) {
+	log.Printf("Expire.%s.Err: %s\n", ctx, err)
+}
+
+func isExpired(md *api.MetaData) bool {
+	dur := md.Expiry.Sub(time.Now()).Round(time.Minute)
+
+	if dur > 0 {
+		return false
+	}
+
+	return true
+}
+
+func Shred(apiS *api.Service, dbS *db.Service) {
+	for {
+		time.Sleep(time.Hour)
+
+		keys, err := dbS.GetAll()
+		if err != nil {
+			logErr("loop", err.Error())
+			continue
+		}
+
+		for _, key := range keys {
+			md, err := apiS.GetMetaData(key)
+			if err != nil {
+				logErr("loop", err.Msg)
+				continue
+			}
+
+			if isExpired(md) {
+				err := apiS.Delete(key)
+				logErr("loop", err.Error())
+			}
+		}
+	}
+}
