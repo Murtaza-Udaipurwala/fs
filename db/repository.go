13e330438f785f54862d794bdd7cc7fc59f78835
Murtaza Udaipurwala <murtaza.u6e24@gmail.com>
2022-03-25 12:12:38 +0530

    return err if key does not exists in DB

--- db/repository.go
+++ db/repository.go
@@ -1,5 +1,9 @@
 package db
 
+import "errors"
+
+var ErrDoesNotExist = errors.New("key does not exist")
+
 func (r *Repo) Set(key string, val []byte) error {
 	_, err := r.conn.Do("SET", key, val)
 	return err
@@ -9,6 +13,10 @@ func (r *Repo) Get(key string) ([]byte, error) {
 	val, err := r.conn.Do("GET", key)
 	if err != nil {
 		return nil, err
+	}
+
+	if val == nil {
+		return nil, ErrDoesNotExist
 	}
 
 	return val.([]byte), nil
